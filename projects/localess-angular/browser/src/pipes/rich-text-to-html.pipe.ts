2f6a0849c8168640bccbc5a1eb77b483e354e3cb
alexcibotari <alexandru.cibotari@gmail.com>
2024-12-09 22:13:41 +0100

    Add RichText support

--- projects/localess-angular/browser/src/pipes/rich-text-to-html.pipe.ts
+++ projects/localess-angular/browser/src/pipes/rich-text-to-html.pipe.ts
@@ -1,13 +1,46 @@
 import {Pipe, PipeTransform} from "@angular/core";
-import {toHtml} from 'hast-util-to-html';
-import type {Nodes, RootContent} from "hast";
+import {JSONContent} from "@tiptap/core";
+import { generateHTML } from '@tiptap/html'
+import Bold from '@tiptap/extension-bold';
+import Italic from '@tiptap/extension-italic';
+import Strike from '@tiptap/extension-strike';
+import Underline from '@tiptap/extension-underline';
+import Document from '@tiptap/extension-document';
+import Text from '@tiptap/extension-text';
+import Paragraph from '@tiptap/extension-paragraph';
+import History from '@tiptap/extension-history';
+import ListItem from '@tiptap/extension-list-item';
+import OrderedList from '@tiptap/extension-ordered-list';
+import BulletList from '@tiptap/extension-bullet-list';
+import Code from '@tiptap/extension-code';
+import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight';
+import Link from '@tiptap/extension-link';
+import Heading from '@tiptap/extension-heading';
 
 @Pipe({
   name: "llRtToHtml",
   standalone: true,
 })
 export class RichTextToHtmlPipe implements PipeTransform {
-  transform(value: Array<RootContent> | Nodes | any): string {
-    return toHtml(value);
+  transform(value: JSONContent | any): string {
+    return generateHTML(value, [
+      Document,
+      Text,
+      Paragraph,
+      Heading.configure({
+        levels: [1, 2, 3, 4, 5, 6],
+      }),
+      Bold,
+      Italic,
+      Strike,
+      Underline,
+      History,
+      ListItem,
+      OrderedList,
+      BulletList,
+      Code,
+      CodeBlockLowlight,
+      Link,
+    ]);
   }
 }
