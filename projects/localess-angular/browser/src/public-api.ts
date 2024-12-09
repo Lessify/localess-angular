9845315ee45d9821f1c08cae137fae25e1bac232
alexcibotari <alexandru.cibotari@gmail.com>
2024-12-09 21:45:19 +0100

    Add RichText support

--- projects/localess-angular/browser/src/public-api.ts
+++ projects/localess-angular/browser/src/public-api.ts
@@ -1,22 +1,24 @@
+// Components
+export * from './components/localess.component';
+
 // Directive
 export * from './directives/content.directive';
 
 // Pipe
 export * from './pipes/asset.pipe';
 export * from './pipes/link.pipe';
+export * from './pipes/rich-text-to-html.pipe';
+export * from './pipes/safe-html.pipe';
 
 // Service
 export * from  './services/asset.service';
 export * from  './services/translation.service';
+
+// Utils
+export * from './utils/link.utils';
 
 // Config
 export * from  './localess.config';
 
 // Providers
 export * from  './localess.providers';
-
-// Components
-export * from './components/localess.component';
-
-// Utils
-export * from './utils/link.utils';
