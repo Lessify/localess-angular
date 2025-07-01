04a1f09b94d2b66b049c3d3a3bb58eee3fcb8a79
alexcibotari <alexandru.cibotari@gmail.com>
2025-07-01 12:09:13 +0200

    update dependency

--- projects/angular-ssr/src/app/app.config.server.ts
+++ projects/angular-ssr/src/app/app.config.server.ts
@@ -1,6 +1,5 @@
+import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { provideServerRouting } from '@angular/ssr';
 import {provideLocalessServer} from '@localess/angular/server';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
@@ -8,21 +7,16 @@ import {LocalessServerService} from './shared/services/localess-server.service';
 import {LocalessService} from './shared/services/localess.service';
 
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering(),
-    provideServerRouting(serverRoutes),
-    provideLocalessServer({
+  providers: [provideServerRendering(withRoutes(serverRoutes)), provideLocalessServer({
       origin: "https://demo.localess.org", // Replace it for your origin
       spaceId: "MmaT4DL0kJ6nXIILUcQF", // Replace it for your spaceId
       version: "draft",
       token: "Y4rvboPnyzVeC7LddEK5", // Replace it for your token
       debug: true,
-    }),
-    {
+    }), {
       provide: LocalessService,
       useClass: LocalessServerService,
-    },
-  ]
+    }]
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
